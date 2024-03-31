0dd3f95542dd245b2d48bd98eb6499887ccaa8a9
NOOTHI AKSHITH <125175973+noothiakshith@users.noreply.github.com>
2024-03-31 12:53:28 +0000

    Remove unused files and update dependencies

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='flex flex-col gap-10 size-full text-white'>
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className='flex h-full flex-col justify-between max:md:px-5 max-md:py-8 lg:p-11'>
+          <h2>Upcoming meeting at: 12:30 PM</h2>
+        </div>
+
+        </div>  
+      </section>
+  )
+}
+
+export default Home
---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonalRoom = () => {
+  return (
+    <section className='flex flex-col gap-10 size-full text-white'>
+    <h1 className='text-3xl font-bold'>PersonalRoom</h1>    
+    </section>
+  )
+}
+
+export default PersonalRoom
